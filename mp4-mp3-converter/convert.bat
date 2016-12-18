e62a22debbcc068a3269e696214a6510a7c01bcf
sonnyky <sonny_yap@hotmail.com>
2016-12-18 23:17:32 +0900

    converter
    move to specified folder after converting to mp3

--- mp4-mp3-converter/convert.bat
+++ mp4-mp3-converter/convert.bat
@@ -1,8 +1,18 @@
 @ECHO OFF
 setlocal enabledelayedexpansion
+set arg1=%1
+
+if "%1"=="" (
+echo "Please specify target folder"
+exit /b
+) else (
+
 for %%f in (*.mp4) do (
   set /p val=<%%f
   echo "fullname: %%f"
   echo "name: %%~nf"
   echo "contents: !val!"
+  ffmpeg -i "%%f" "%%~nf.mp3"
+  move "%%~nf.mp3" %1
+)
 )
